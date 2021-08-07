c673c0c33adefcb9ce817d6141cea28f951b7322
Stephan Klinger <staeff@arcor.de>
2021-08-07 21:51:08 +0200

    Add state that holds all the clicks as char

--- src/App.js
+++ src/App.js
@@ -3,21 +3,36 @@ import React, {useState} from 'react'
 const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
+  // Initialise all clicks as empty array to store
+  // every click as a char.
+  const [allClicks, setAll] = useState([])
 
-  const setLeftAction = () => setLeft(left + 1)
-  const setRightAction = () => setRight(right + 1)
+  // .concat does not mute an array but rather returns a
+  // copy of the array. Its important not mutate state directly
+  // in React. push would update the existing array, so it
+  // should not be used for this
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
 
 
   return (
     <div>
       {left}
-      <button onClick={setLeftAction}>
+      <button onClick={handleLeftClick}>
         left
       </button>
-      <button onClick={setRightAction}>
+      <button onClick={handleRightClick}>
         right
       </button>
       {right}
+      <p>{allClicks.join(' ')}</p>
     </div>
   )
 }
