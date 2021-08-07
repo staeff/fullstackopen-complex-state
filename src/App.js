c001e556dae3757fe749b3ace589cb6188fa5395
Stephan Klinger <staeff@arcor.de>
2021-08-07 16:17:19 +0200

    Initial app with a complex state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
 
-function App() {
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+
+  const setLeftAction = () => setLeft(left + 1)
+  const setRightAction = () => setRight(right + 1)
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {left}
+      <button onClick={setLeftAction}>
+        left
+      </button>
+      <button onClick={setRightAction}>
+        right
+      </button>
+      {right}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
