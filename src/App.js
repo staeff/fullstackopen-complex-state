0c94a825dc77676feae62698f583660f54445386
Stephan Klinger <staeff@arcor.de>
2021-08-07 21:39:52 +0200

    Go back to complex state to continue with tutorial

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
 import React, {useState} from 'react'
 
 const App = () => {
-  const [clicks, setClicks] = useState({
-    left:0, right:0
-  })
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
 
-  const handleLeftClick = () =>
-    setClicks ({...clicks, left: clicks.left + 1,
-    })
+  const setLeftAction = () => setLeft(left + 1)
+  const setRightAction = () => setRight(right + 1)
 
-  const handleRightClick = () =>
-    setClicks ({...clicks, right: clicks.right + 1,
-    })
 
   return (
     <div>
-      {clicks.left}
-      <button onClick={handleLeftClick}>
+      {left}
+      <button onClick={setLeftAction}>
         left
       </button>
-      <button onClick={handleRightClick}>
+      <button onClick={setRightAction}>
         right
       </button>
-      {clicks.right}
+      {right}
     </div>
   )
 }
